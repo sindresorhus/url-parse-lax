f42faaed86ad5099ae7ade8d07373f893758eade
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 19:04:49 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,10 +1,8 @@
-'use strict';
-var test = require('ava');
-var fn = require('./');
+import test from 'ava';
+import fn from './';
 
-test(function (t) {
+test(t => {
 	t.is(fn('sindresorhus.com').hostname, 'sindresorhus.com');
 	t.is(fn('192.168.0.1:80').hostname, '192.168.0.1');
 	t.is(fn('[2001:db8::]:80').hostname, '2001:db8::');
-	t.end();
 });
