79aee260232deb0bf8b1e33db658a6b421af1e4f
Gilles De Mey <gilles.de.mey@gmail.com>
2017-10-06 16:49:10 +0200

    pass options to prepend-http

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 const url = require('url');
 const prependHttp = require('prepend-http');
 
-module.exports = x => {
+module.exports = (x, opts) => {
 	if (typeof x !== 'string') {
 		throw new TypeError(`Expected \`url\` to be of type \`string\`, got \`${typeof x}\` instead.`);
 	}
 
-	const withProtocol = prependHttp(x);
+	const withProtocol = prependHttp(x, opts);
 	const parsed = url.parse(withProtocol);
 
 	if (withProtocol !== x) {
